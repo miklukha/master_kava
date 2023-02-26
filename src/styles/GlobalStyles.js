8fd52c69a09610438c08272aee9e73b2915783d9
Anna Miklukha <miklukha.anna@ukr.net>
2023-02-26 20:37:14 +0100

    change: changes globalStyles

--- src/styles/GlobalStyles.js
+++ src/styles/GlobalStyles.js
@@ -1,71 +1,70 @@
 import { css } from '@emotion/react';
 import { colors } from './utils/variables';
 
-export const globalStyles = () =>
-  css`
-    html {
-      box-sizing: border-box;
-      scroll-behavior: smooth;
-      /* width: 100vw;
+export const globalStyles = css`
+  html {
+    box-sizing: border-box;
+    scroll-behavior: smooth;
+    /* width: 100vw;
     overflow-x: hidden; */
-    }
+  }
 
-    *,
-    *::after,
-    *::before {
-      box-sizing: inherit;
-    }
+  *,
+  *::after,
+  *::before {
+    box-sizing: inherit;
+  }
 
-    body {
-      font-family: 'Roboto', sans-serif;
-      /* min-height: 100vh;
+  body {
+    font-family: 'Roboto', sans-serif;
+    /* min-height: 100vh;
     margin: 0; */
-      /* font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto',
+    /* font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto',
       'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans',
       'Helvetica Neue', sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale; */
-      color: ${colors.mainText};
-      background-color: ${colors.mainBg};
-    }
+    color: ${colors.mainText};
+    background-color: ${colors.mainBg};
+  }
 
-    h1,
-    h2,
-    h3,
-    h4,
-    h5,
-    h6,
-    p {
-      margin: 0;
-    }
+  h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  h6,
+  p {
+    margin: 0;
+  }
 
-    img {
-      display: block;
-      max-width: 100%;
-      height: auto;
-    }
+  img {
+    display: block;
+    max-width: 100%;
+    height: auto;
+  }
 
-    address {
-      font-style: normal;
-    }
+  address {
+    font-style: normal;
+  }
 
-    button {
-      padding: 0;
-      border: none;
-      font: inherit;
-      color: inherit;
-      background-color: transparent;
-      cursor: pointer;
-    }
+  button {
+    padding: 0;
+    border: none;
+    font: inherit;
+    color: inherit;
+    background-color: transparent;
+    cursor: pointer;
+  }
 
-    ul {
-      list-style: none;
-      padding-left: 0;
-      margin: 0;
-    }
+  ul {
+    list-style: none;
+    padding-left: 0;
+    margin: 0;
+  }
 
-    a {
-      text-decoration: none;
-      color: inherit;
-    }
-  `;
+  a {
+    text-decoration: none;
+    color: inherit;
+  }
+`;
