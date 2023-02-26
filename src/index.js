618952ac91cfc475f6d989318bd8ec83324938cc
Anna Miklukha <miklukha.anna@ukr.net>
2023-02-26 20:49:01 +0100

    change: changes globalStyles

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import { Global } from '@emotion/react';
 import 'modern-normalize';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { globalStyles } from 'styles/global';
+import { globalStyles } from 'styles/globalStyles';
 import { App } from 'App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
