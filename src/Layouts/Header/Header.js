7d07eed9263726ab232a5ee703103286feb7aabf
Anna Miklukha <miklukha.anna@ukr.net>
2023-02-26 15:08:05 +0100

    create: creates the folder layouts and header's style

---
+++ src/Layouts/Header/Header.js
@@ -0,0 +1,12 @@
+import { HeaderStyled } from './Header.styled';
+import { Container } from 'components';
+
+export function Header() {
+  return (
+    <>
+      <HeaderStyled>
+        <Container></Container>
+      </HeaderStyled>
+    </>
+  );
+}
---
+++ src/Layouts/Header/Header.styled.js
@@ -0,0 +1,9 @@
+import styled from '@emotion/styled';
+import { colors } from 'styles/utils/variables';
+
+export const HeaderStyled = styled.header`
+  height: 80px;
+  width: 100vw;
+
+  background-color: ${colors.headerBg};
+`;
